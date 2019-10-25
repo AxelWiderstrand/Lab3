d754be69cc9abeca6c80b15c9abfcbd351902b33
AxelWiderstrand <55433792+AxelWiderstrand@users.noreply.github.com>
2019-10-25 13:19:49 +0200

    Update Famous.hs

--- Famous.hs
+++ Famous.hs
@@ -1,8 +1,9 @@
+import System.IO.Error
+import System.IO
+import Test.QuickCheck
+
 data QA = Question String QA QA | Action String 
     deriving (Show, Read)
-
---read :: String -> QA
---read s = 
 
 
 isfrEurope = Question "Is she from Europe?" isScientist isActress
@@ -13,51 +14,42 @@ hillary = Question "Is it Hillary Clinton?" yes no
 marie = Question "Is it Marie Curie?" yes no
 queen = Question "Is it Queen Elisabeth 2" yes no
 yes = Action "Yes!" 
-no = NewQ "Who was it?" q1
-q1 = NewQ "Give me a question for the famous person" q2
-q2 = 
-
-s <- getline 
+no = Action "No" 
 
 
-play :: QA -> IO ()
+play :: QA -> IO QA
 play (Question q ifYes ifNo) = do putStrLn q
                                   answer <- getLine
                                   case answer of
-                                    'y':_ -> play ifYes
-                                    'n':_ -> play ifNo
+                                    'y':_ -> do newYes <- play ifYes
+                                                return (Question q newYes ifNo)
+                                    'n':_ -> do newNo <- play ifNo
+                                                return (Question q ifYes newNo)
                                     _     -> play (Question q ifYes ifNo)
 play (Action a)            = do 
-    foo <- yesNoQuestion ....
+    foo <- yesNoQuestion  ("Is it " ++ a ++ "?")
     if foo
-    then return (Action a)
+    then do putStrLn "I won!"
+            return (Action a)
     else do
-        q <- ......
-        ans <- .......
-        return (Question q (Action ans) (Action a))
-  
-                                  --play d
---play Done                    = return ()
-
-
-tryIOError :: IO a -> IO (Either IOError a)
-tryIOError i = undefined
-
+        ans <- question "Just curious: Who was your famous person?"
+        q2 <- question ("Give me a question for which the answer for " ++ ans ++ " is yes and the answer for " ++ a ++ "is no")
+        return (Question q2 (Action ans) (Action a))
 
 question :: String -> IO String
-question s = undefined
+question s = do putStrLn s
+                answer <- getLine
+                return answer
     
-    
-{-    
-    do putStrLn "q"
-       q <- readFile "Questions.hs"
-       s <- getLine
-       return s
--}
 
 yesNoQuestion :: String -> IO Bool
-yesNoQuestion s = undefined
+yesNoQuestion s = do
+                      answer <- question s
+                      case answer of
+                       'y':_ -> return True
+                       'n':_ -> return False
+                       _     -> yesNoQuestion s
+                        
 
-
-
---main :: IO ()
+main :: IO ()
+main = undefined
