086e372d4f9cd33e5a3b39d59b3b851db4f26822
AxelWiderstrand <55433792+AxelWiderstrand@users.noreply.github.com>
2019-10-25 15:58:02 +0200

    Update Famous.hs

--- Famous.hs
+++ Famous.hs
@@ -1,22 +1,23 @@
 import System.IO.Error
 import System.IO
-import Test.QuickCheck
 
+--Data type for a question-answer tree
 data QA = Question String QA QA | Action String 
     deriving (Show, Read)
 
 
-isfrEurope = Question "Is she from Europe?" isScientist isActress
-isActress = Question "Is she an actress" marilyn hillary
+--This is a default tree    
+defaultQA   = isfrEurope
+isfrEurope  = Question "Is she from Europe?" isScientist isActress
+isActress   = Question "Is she an actress" marilyn hillary
 isScientist = Question "Is she a scientist" marie queen 
-marilyn = Question "Is it Marilyn Monroe?" yes no
-hillary = Question "Is it Hillary Clinton?" yes no
-marie = Question "Is it Marie Curie?" yes no
-queen = Question "Is it Queen Elisabeth 2" yes no
-yes = Action "Yes!" 
-no = Action "No" 
+marilyn     = Action "Marilyn Monroe"
+hillary     = Action "Hillary Clinton"
+marie       = Action "Marie Curie"
+queen       = Action "Queen Elisabeth 2"
 
 
+--Plays a QA tree
 play :: QA -> IO QA
 play (Question q ifYes ifNo) = do putStrLn q
                                   answer <- getLine
@@ -27,21 +28,26 @@ play (Question q ifYes ifNo) = do putStrLn q
                                                 return (Question q ifYes newNo)
                                     _     -> play (Question q ifYes ifNo)
 play (Action a)            = do 
-    foo <- yesNoQuestion  ("Is it " ++ a ++ "?")
+    foo <- yesNoQuestion  ("My guess: Is it " ++ a ++ "?")
     if foo
-    then do putStrLn "I won!"
+    then do putStrLn "Hurray I won!"
             return (Action a)
     else do
+        putStrLn "OK - you won this time"
         ans <- question "Just curious: Who was your famous person?"
-        q2 <- question ("Give me a question for which the answer for " ++ ans ++ " is yes and the answer for " ++ a ++ "is no")
+        q2  <- question ("Give me a question for which the answer for " 
+                          ++ ans ++ " is yes and the answer for " ++ a ++ " is no")
         return (Question q2 (Action ans) (Action a))
 
+
+--Function used when asking questions
 question :: String -> IO String
 question s = do putStrLn s
                 answer <- getLine
                 return answer
     
 
+--Function used when asking yes/no questions
 yesNoQuestion :: String -> IO Bool
 yesNoQuestion s = do
                       answer <- question s
@@ -51,5 +57,18 @@ yesNoQuestion s = do
                        _     -> yesNoQuestion s
                         
 
+--Used for running the game
 main :: IO ()
-main = undefined
+main = do
+         s <- tryIOError (readFile "Questions")
+         let tree = case s of
+                         Left e   -> defaultQA
+                         Right [] -> defaultQA
+                         Right c  -> read c
+         newTree  <- play tree
+         writeFile "Questions" (show newTree)
+         again    <- yesNoQuestion "Do you want to play again?"   
+         if again
+         then do putStrLn "Great!"
+                 main
+         else do putStrLn "Bye!"
